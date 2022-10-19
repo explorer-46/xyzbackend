b9836891fae0abeaaa5318f3bfc2aae66d5ad268
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-19 08:53:21 +0530

    master

---
+++ src/main/java/com/nestdigital/xyzbackendapp/XyzbackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzbackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzbackendAppApplication.class, args);
+	}
+
+}
